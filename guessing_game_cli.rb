236fd31b31b2ef50eb1d101c6f35148cd09fc882
Joanna E K <kalemajoanna@gmail.com>
2019-11-17 12:25:35 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,8 +3,7 @@ def run_guessing_game
   random_guess = rand(1..6)
   gets 
   puts "Goodbye!"
-   if rand > 6
-    gets
+   if rand > 5
     puts "Sorry! The computer guessed <6>."
     elsif
     rand < 6 
