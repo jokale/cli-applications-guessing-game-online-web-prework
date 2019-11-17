cf12bd73dcb39eb5b5f752bd4043e6541b22d086
Joanna E K <kalemajoanna@gmail.com>
2019-11-17 14:08:52 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,7 +4,7 @@ computers_guess = rand(6) + 1
 user_input = gets.chomp 
 if user_input == "exit"
   puts "Goodbye!"
-  elsif user_input ==computers_guess
+  elsif user_input.to_i ==computers_guess
 print "You guessed the correct number!"
 else
 print "Sorry! The computer guessed #{computers_guess}."
