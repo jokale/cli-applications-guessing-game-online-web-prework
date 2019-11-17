d5e6c69991d6a13781e8ef272fd4700a22d90155
Joanna E K <kalemajoanna@gmail.com>
2019-11-17 13:41:58 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,14 +1,24 @@
 # Code your solution here!
-def run_guessing_game
-  random_guess = rand(1..6)
-  gets 
-  puts "Goodbye!"
-   if rand > 5
-    puts "Sorry! The computer guessed <6>."
-    elsif
-    rand < 6 
-    puts "You guessed the correct number!"
- end  
- end 
+# def run_guessing_game
+#   random_guess = rand(1..6)
+#   gets 
+#   puts "Goodbye!"
+#   if rand > 5
+#     puts "Sorry! The computer guessed <6>."
+#     elsif
+#     rand < 6 
+#     puts "You guessed the correct number!"
+# end  
+# end 
   
-
+def run_guessing_game
+  computer_random_guess = rand() 
+  user_input = gets.chomp
+  if user_input == 'exit'
+    puts 'Goodbye!'
+  elsif user_input.to_i == computer_random_guess
+    print 'You guessed the correct number!'
+  else
+    print "Sorry! The computer guessed #{computer_random_guess}."
+  end
+end
